733b1d5104a821973ec5986539576965235b78f7
Erwin M. Galura <59816319+errwwin25@users.noreply.github.com>
2021-05-20 18:04:57 +0800

    Add files via upload

---
+++ DataBaseContext.cs
@@ -0,0 +1,26 @@
+﻿using CRUD.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace CRUD.Services
+{
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<EmployeeModel> Employees { get; set; }
+        public object Employees { get; internal set; }
+
+        public DatabaseContext()
+        {
+            this.Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string dbPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal), "Employee.db");
+            optionsBuilder.UseSqlite($"Filename={dbPath}");
+        }
+    }
+}
